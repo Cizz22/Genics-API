6c8b335bdffcf97b34e482edf9386d16755bbd90
Cizz22 <cisatraa@gmail.com>
2023-06-25 16:47:08 +0700

    update api

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,10 +8,40 @@ const getProducts = async (req, res) => {};
 const saveProduct = async (req, res) => {};
 
 // Update a product
-const updateProduct = async (req, res) => {};
+const updateProduct = async (req, res) => {
+  const productId = req.params.id
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, {
+      new: true
+    })
+
+    res.status(200).json({
+      message: "Update product success",
+      data: updatedProduct
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "Update product failed",
+      data: error
+    })
+  }
+
+};
 
 // Delete a product
-const deleteProduct = async (req, res) => {};
+const deleteProduct = async (req, res) => {
+  const productId = req.params.id
+
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(productId)
+
+    res.status(200).json({
+      message: "Delete product success",
+    })
+  } catch (error) {
+
+  }
+};
 
 module.exports = {
   getProducts,
